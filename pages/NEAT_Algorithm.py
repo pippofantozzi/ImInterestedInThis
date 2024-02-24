ff3f4f6b1c2699776f8f3f5ba97a3be9fd11ca2e
pippofantozzi <106916039+pippofantozzi@users.noreply.github.com>
2024-02-24 03:15:49 +0000

    Update NEAT_Algorithm.py

--- pages/NEAT_Algorithm.py
+++ pages/NEAT_Algorithm.py
@@ -1,5 +1,10 @@
 import streamlit as st
 from texts.textNEAT import get_images, process_text
+st.set_page_config(
+    page_title="Im Interested In NEAT",
+    page_icon="🔍",
+    
+)
 
 st.title("🐣 The NEAT Algorithm")
 st.subheader("How Natural Selection and Evolution can help us make better AI")
